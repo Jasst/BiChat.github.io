0b00a6ce5af81857f16635de1ff2250b480fb307
Jasst <nel.tern@mail.ru>
2024-05-31 01:16:21 +0300

    Vercel

--- app.py
+++ app.py
@@ -4,11 +4,8 @@ import subprocess
 from flask import Flask, jsonify, request, render_template
 from mnemonic import Mnemonic
 from cryptography.fernet import Fernet
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
-
 
 mnemonic = Mnemonic('english')
 cipher_suite = Fernet(Fernet.generate_key())
