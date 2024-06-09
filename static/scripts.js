456eadac624c725d66e7d7129e8557aa9cf2d1a5
Jasst <nel.tern@mail.ru>
2024-06-09 16:10:11 +0300

    Update scripts.js

--- static/scripts.js
+++ static/scripts.js
@@ -22,7 +22,6 @@ document.addEventListener('click', function(event) {
     }
 });
 
-
 function createWallet() {
     fetch(`/create_wallet?lang=${currentLanguage}`, {
         method: 'POST',
@@ -177,9 +176,6 @@ function displayDialog(messages, recipient) {
     const dialogContainer = document.getElementById('current-dialog');
     dialogContainer.innerHTML = '';
 
-    // Записываем адрес получателя в поле "Recipient Address" только при выборе диалогового окна
-    document.getElementById('recipient').value = recipient;
-
     messages.forEach(message => {
         const messageElement = document.createElement('div');
         messageElement.classList.add('message');
@@ -192,10 +188,18 @@ function displayDialog(messages, recipient) {
         messageElement.innerHTML = `
             <div class="message-content">${message.content}</div>
             <div class="message-sender">From: ${message.sender}</div>
+            <div class="message-recipient">To: ${message.recipient}</div>
             <div class="message-timestamp">${timestamp}</div>
         `;
         dialogContainer.appendChild(messageElement);
     });
+
+    // Копируем адрес получателя в поле отправителя
+    copyRecipientAddress(recipient);
+}
+
+function copyRecipientAddress(recipient) {
+    document.getElementById('recipient').value = recipient;
 }
 
 function toggleTheme() {
@@ -258,58 +262,4 @@ function switchLanguage() {
 
     const translations = {
         en: {
-            address_label: "Address:",
-            logout_button: "Logout",
-            show_mnemonic_button: "Show Mnemonic Phrase",
-            hide_mnemonic_button: "Hide Mnemonic",
-            toggle_visibility_button: "Toggle Visibility",
-            title: "Blockchain Messenger",
-            toggle_theme: "Toggle Theme",
-            create_wallet: "Create Wallet",
-            login_button: "Login",
-            send_message: "Send Message",
-            get_messages: "Get Messages",
-            wallet_section: "Create Wallet or Login",
-            send_message_section: "Send Message",
-            chat_section: "Chat",
-            mnemonic_label: "Mnemonic Phrase:",
-            recipient_label: "Recipient Address:",
-            content_label: "Message:",
-            send_button: "Send Message",
-            get_messages_button: "Get Messages"
-        },
-        ru: {
-            address_label: "Адрес:",
-            logout_button: "Выход",
-            show_mnemonic_button: "Показать мнемоническую фразу",
-            hide_mnemonic_button: "Спрятать мнемоническую фразу",
-            toggle_visibility_button: "Разблокировать/заблокировать",
-            title: "Блокчейн Мессенджер",
-            toggle_theme: "Переключить тему",
-            create_wallet: "Создать кошелек",
-            login_button: "Войти",
-            send_message: "Отправить сообщение",
-            get_messages: "Получить сообщения",
-            wallet_section: "Создать кошелек или Войти",
-            send_message_section: "Отправить сообщение",
-            chat_section: "Чат",
-            mnemonic_label: "Мнемоническая фраза:",
-            recipient_label: "Адрес получателя:",
-            content_label: "Сообщение:",
-            send_button: "Отправить сообщение",
-            get_messages_button: "Получить сообщения"
-        }
-    };
-
-    const selectedTranslations = translations[currentLanguage];
-    const elementsToTranslate = document.querySelectorAll('[data-translate]');
-    elementsToTranslate.forEach(element => {
-        const translationKey = element.dataset.translate;
-        if (translationKey) {
-            element.innerText = selectedTranslations[translationKey];
-        }
-    });
-}
-
-
-  
+            address_label
