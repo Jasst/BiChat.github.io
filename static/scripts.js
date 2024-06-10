45a1732506b84f3f63d9b6f63267f3873b27dc55
Jasst <nel.tern@mail.ru>
2024-06-10 23:00:45 +0300

    Стабильность локализация

--- static/scripts.js
+++ static/scripts.js
@@ -89,7 +89,6 @@ document.addEventListener('click', function(event) {
     }
 });
 
-
 function toggleTheme() {
     document.body.classList.toggle('dark-theme');
     state.theme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
@@ -119,38 +118,6 @@ async function createWallet() {
     } catch (error) {
         console.error('Error:', error);
         showAlert('Error creating wallet');
-    }
-}
-
-async function loginWallet() {
-    try {
-        const mnemonic = document.getElementById('mnemonic-login').value;
-
-        const response = await fetch(`/login_wallet?lang=${state.currentLanguage}`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ mnemonic_phrase: mnemonic })
-        });
-        const data = await response.json();
-
-        state.mnemonicPhrase = mnemonic;
-        state.userAddress = data.address;
-
-        document.getElementById('wallet-section').style.display = 'none';
-        document.getElementById('create-wallet-container').style.display = 'none';
-        document.getElementById('login-status').innerHTML = data.message;
-        document.getElementById('login-wallet-container').style.display = 'none';
-        document.getElementById('send-message-section').style.display = 'block';
-        document.getElementById('chat-section').style.display = 'block';
-        document.getElementById('logout-button').style.display = 'block';
-
-        saveState();
-
-        checkIncomingMessages();
-        await getMessages();
-    } catch (error) {
-        console.error('Error:', error);
-        showAlert('Error logging in');
     }
 }
 
@@ -194,9 +161,41 @@ async function sendMessage() {
         showAlert('Error sending message');
     }
 }
+async function loginWallet() {
+    try {
+        const mnemonic = document.getElementById('mnemonic-login').value;
+
+        const response = await fetch(`/login_wallet?lang=${state.currentLanguage}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ mnemonic_phrase: mnemonic })
+        });
+        const data = await response.json();
+
+        state.mnemonicPhrase = mnemonic;
+        state.userAddress = data.address;
+
+        document.getElementById('wallet-section').style.display = 'none';
+        document.getElementById('create-wallet-container').style.display = 'none';
+        document.getElementById('login-status').innerHTML = data.message;
+        document.getElementById('login-wallet-container').style.display = 'none';
+        document.getElementById('send-message-section').style.display = 'block';
+        document.getElementById('chat-section').style.display = 'block';
+        document.getElementById('logout-button').style.display = 'block';
+
+        saveState();
+
+        checkIncomingMessages();
+        await getMessages();
+    } catch (error) {
+        console.error('Error:', error);
+        showAlert('Error logging in');
+    }
+}
 
 
-async function getMessages() {
+
+async function getMessages(recipientAddress) {
     try {
         const response = await fetch(`/get_messages?lang=${state.currentLanguage}`, {
             method: 'POST',
@@ -242,7 +241,16 @@ async function getMessages() {
             }
         }
 
-        if (!state.activeDialog) {
+        // Проверяем, есть ли переданный адрес получателя и отображаем соответствующий диалог
+        if (recipientAddress) {
+            const dialogKey = `${state.userAddress}_${recipientAddress}`;
+            if (dialogs.hasOwnProperty(dialogKey)) {
+                state.activeDialog = dialogKey;
+                displayDialog(dialogs[dialogKey], recipientAddress);
+                saveState();
+            }
+        } else if (!state.activeDialog) {
+            // Если нет активного диалога, выбираем первый диалог
             const firstDialogKey = Object.keys(dialogs)[0];
             if (firstDialogKey) {
                 const [sender, recipient] = firstDialogKey.split('_');
@@ -262,9 +270,10 @@ async function getMessages() {
         }
     } catch (error) {
         console.error('Error:', error);
-        showAlert('Error fetching messages');
+        //showAlert('Error fetching messages');
     }
 }
+
 
 function displayDialog(messages, recipient) {
     const dialogContainer = document.getElementById('current-dialog');
@@ -299,70 +308,6 @@ function copyRecipientAddress(recipient) {
     getMessages().then(r => {});
 }
 
-function switchLanguage() {
-    const languageToggle = document.getElementById('language-toggle');
-    state.currentLanguage = state.currentLanguage === 'en' ? 'ru' : 'en';
-    languageToggle.innerText = state.currentLanguage === 'en' ? 'Switch to Russian' : 'Переключить на английский';
-
-    const translations = {
-        en: {
-            address_label: "Address:",
-            logout_button: "Logout",
-            show_mnemonic_button: "Show Mnemonic Phrase",
-            hide_mnemonic_button: "Hide Mnemonic",
-            toggle_visibility_button: "Toggle Visibility",
-            title: "Blockchain Messenger",
-            toggle_theme: "Toggle Theme",
-            create_wallet: "Create Wallet",
-            login_button: "Login",
-            send_message: "Send Message",
-            get_messages: "Get Messages",
-            wallet_section: "Create Wallet or Login",
-            send_message_section: "Send Message",
-            chat_section: "Chat",
-            mnemonic_label: "Mnemonic Phrase:",
-            recipient_label: "Recipient Address:",
-            content_label: "Message:",
-            send_button: "Send Message",
-            get_messages_button: "Get Messages"
-        },
-        ru: {
-            address_label: "Адрес:",
-            logout_button: "Выход",
-            show_mnemonic_button: "Показать мнемоническую фразу",
-            hide_mnemonic_button: "Спрятать мнемоническую фразу",
-            toggle_visibility_button: "Разблокировать/заблокировать",
-            title: "Блокчейн Мессенджер",
-            toggle_theme: "Переключить тему",
-            create_wallet: "Создать кошелек",
-            login_button: "Войти",
-            send_message: "Отправить сообщение",
-            get_messages: "Получить сообщения",
-            wallet_section: "Создать кошелек или Войти",
-            send_message_section: "Отправить сообщение",
-            chat_section: "Чат",
-            mnemonic_label: "Мнемоническая фраза:",
-            recipient_label: "Адрес получателя:",
-            content_label: "Сообщение:",
-            send_button: "Отправить сообщение",
-            get_messages_button: "Получить сообщения"
-        }
-    };
-
-    const selectedTranslations = translations[state.currentLanguage];
-    const elementsToTranslate = document.querySelectorAll('[data-translate]');
-    elementsToTranslate.forEach(element => {
-        const translationKey = element.dataset.translate;
-        if (translationKey) {
-            element.innerText = selectedTranslations[translationKey];
-        }
-    });
-
-    localStorage.setItem('currentLanguage', state.currentLanguage); // Update stored language
-    saveState(); // Save state after changing language
-}
-
-
 function logout() {
     hideMnemonic();
     state.mnemonicPhrase = '';
@@ -380,7 +325,7 @@ function logout() {
 function showMnemonic() {
     // Добавляем сохранение состояния кнопки при ее клике
     localStorage.setItem('showMnemonic', 'true');
-    
+
     const walletInfo = document.getElementById('wallet-info');
     walletInfo.style.display = 'block';
     walletInfo.innerHTML = `<label data-translate="address_label">Address:</label>
@@ -401,7 +346,7 @@ function hideMnemonic() {
     // Добавляем сохранение состояния кнопки при ее клике
     localStorage.setItem('showMnemonic', 'false');
 
-    
+
     const walletInfo = document.getElementById('wallet-info');
     walletInfo.style.display = 'none';
 
@@ -432,4 +377,5 @@ function checkIncomingMessages() {
         }
     }, 5000);
 }
+
 
