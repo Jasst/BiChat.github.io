49e030a56048220c1da16e63356852988fedfcc7
Jasst <nel.tern@mail.ru>
2024-06-11 09:57:58 +0300

    Create state.js

---
+++ static/state.js
@@ -0,0 +1,19 @@
+export let state = {
+    mnemonicPhrase: '',
+    userAddress: '',
+    currentLanguage: 'en',
+    activeDialog: '',
+    theme: 'light'
+};
+
+export function saveState() {
+    localStorage.setItem('appState', JSON.stringify(state));
+}
+
+export function loadState() {
+    const storedState = localStorage.getItem('appState');
+    if (storedState) {
+        const parsedState = JSON.parse(storedState);
+        state = { ...state, ...parsedState };
+    }
+}
