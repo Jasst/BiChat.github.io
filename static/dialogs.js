0286ed67a08928cc3dbc225ea75804d17e2b13b0
Jasst <nel.tern@mail.ru>
2024-06-11 10:13:06 +0300

    Update dialogs.js

--- static/dialogs.js
+++ static/dialogs.js
@@ -1,6 +1,6 @@
 import { state, saveState } from './state.js';
 
-export async function getMessages(recipientAddress) {
+export async function getMessages() {
     try {
         const response = await fetch(`/get_messages?lang=${state.currentLanguage}`, {
             method: 'POST',
@@ -15,96 +15,38 @@ export async function getMessages(recipientAddress) {
         const dialogContainer = document.getElementById('current-dialog');
         dialogContainer.innerHTML = '';
 
-        const dialogs = {};
-        data.forEach(message => {
-            const sender = message.sender;
-            const recipient = message.recipient;
-            const [currentAddress, otherAddress] = state.userAddress === sender ? [sender, recipient] : [recipient, sender];
-            const dialogKey = `${currentAddress}_${otherAddress}`;
+        data.dialogs.forEach(dialog => {
+            const dialogTab = document.createElement('div');
+            dialogTab.className = 'dialog-tab';
+            dialogTab.innerText = dialog.address;
+            dialogTab.onclick = () => {
+                state.activeDialog = dialog.address;
+                saveState();
+                updateCurrentDialog();
+            };
 
-            if (!dialogs[dialogKey]) {
-                dialogs[dialogKey] = [];
-            }
-
-            dialogs[dialogKey].push(message);
+            dialogTabs.appendChild(dialogTab);
         });
 
-        for (const dialogKey in dialogs) {
-            if (dialogs.hasOwnProperty(dialogKey)) {
-                const dialogMessages = dialogs[dialogKey];
-                const [sender, recipient] = dialogKey.split('_');
-
-                const tabButton = document.createElement('button');
-                tabButton.textContent = `Dialog with ${recipient}`;
-                tabButton.onclick = function () {
-                    state.activeDialog = dialogKey;
-                    displayDialog(dialogMessages, recipient);
-                    copyRecipientAddress(recipient);
-                    saveState();
-                };
-                dialogTabs.appendChild(tabButton);
-            }
-        }
-
-        if (recipientAddress) {
-            const dialogKey = `${state.userAddress}_${recipientAddress}`;
-            if (dialogs.hasOwnProperty(dialogKey)) {
-                state.activeDialog = dialogKey;
-                displayDialog(dialogs[dialogKey], recipientAddress);
-                saveState();
-            }
-        } else if (!state.activeDialog) {
-            const firstDialogKey = Object.keys(dialogs)[0];
-            if (firstDialogKey) {
-                const [sender, recipient] = firstDialogKey.split('_');
-                state.activeDialog = firstDialogKey;
-                displayDialog(dialogs[firstDialogKey], recipient);
-                copyRecipientAddress(recipient);
-                saveState();
-            }
-        } else {
-            const [sender, recipient] = state.activeDialog.split('_');
-            displayDialog(dialogs[state.activeDialog], recipient);
-        }
-
-        if (localStorage.getItem('activeDialog')) {
-            state.activeDialog = localStorage.getItem('activeDialog');
-            saveState();
-        }
+        updateCurrentDialog();
     } catch (error) {
         console.error('Error:', error);
+        showAlert('Error fetching messages');
     }
 }
 
-export function displayDialog(messages, recipient) {
+function updateCurrentDialog() {
     const dialogContainer = document.getElementById('current-dialog');
     dialogContainer.innerHTML = '';
 
-    messages.forEach(message => {
-        const { sender, recipient, content, timestamp } = message;
-        const messageElement = document.createElement('div');
-        messageElement.classList.add('message');
-        if (sender === state.userAddress) {
-            messageElement.classList.add('sent');
-        } else {
-            messageElement.classList.add('received');
-        }
-        const formattedTimestamp = new Date(timestamp * 1000).toLocaleString();
-        messageElement.innerHTML = `
-            <div class="message-content">${content}</div>
-            <div class="message-sender">From: ${shortenAddressForDisplay(sender)}</div>
-            <div class="message-recipient">To: ${shortenAddressForDisplay(recipient)}</div>
-            <div class="message-timestamp">${formattedTimestamp}</div>
-        `;
-        dialogContainer.appendChild(messageElement);
-    });
-}
+    const dialog = state.dialogs.find(d => d.address === state.activeDialog);
 
-function shortenAddressForDisplay(address) {
-    return address.slice(0, 6) + '...' + address.slice(-4);
-}
-
-export function copyRecipientAddress(recipient) {
-    document.getElementById('recipient').value = recipient;
-    getMessages().then(r => {});
+    if (dialog) {
+        dialog.messages.forEach(message => {
+            const messageElement = document.createElement('div');
+            messageElement.className = 'message';
+            messageElement.innerText = message.content;
+            dialogContainer.appendChild(messageElement);
+        });
+    }
 }
