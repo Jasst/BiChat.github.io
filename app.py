f28260fe3a96dc9ef1b177aaca8b63df0b25c418
Jasst <nel.tern@mail.ru>
2024-06-14 09:38:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,9 +3,25 @@ from translations import translations
 from mnemonic import Mnemonic
 from flask import Flask, jsonify, request, render_template
 from flask_babel import Babel, gettext
-from blockchain import Blockchain , CriptoManager
+from blockchain import Blockchain
 
 from cryptography.fernet import Fernet
+
+
+class CryptoManager:
+    def __init__(self, key):
+        self.key = key
+        self.cipher_suite = Fernet(key)
+
+    def encrypt_message(self, message):
+        encrypted_message = self.cipher_suite.encrypt(message.encode())
+        return encrypted_message.decode()
+
+    def decrypt_message(self, encrypted_message):
+        decrypted_message = self.cipher_suite.decrypt(encrypted_message.encode())
+        return decrypted_message.decode()
+
+
 # Генерируем случайный ключ key = Fernet.generate_key()
 
 key = b'U_Urs-adepKN6SnJt1YI_JasstmeWtyyTNno2UeX_-0='
